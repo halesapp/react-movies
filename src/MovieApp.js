45e5d14081eb0ee94dbca1e0c64cc31c0774b7ff
Riley Hales <rileyhales1@gmail.com>
2021-06-06 11:29:32 -0600

    random button and style improvements

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -5,10 +5,10 @@ import {URL_dbJSON} from "./urls";
 function MovieApp() {
     const [db, setDb] = useState({})
     const [list, setList] = useState([])
-    const [inputOptions, setInputOptions] = useState([])
     const [posters, setPosters] = useState([])
     const [posterIsVisible, setPosterIsVisible] = useState([])
     const [buttons, setButtons] = useState([])
+    const [searchBarValue, setSearchBarValue] = useState("")
 
     useEffect(() => {
         fetch(URL_dbJSON)
@@ -18,16 +18,17 @@ function MovieApp() {
             .then(res => {
                 setDb(res)
                 setList(Object.keys(res))
+                setButtons(() => {
+                    return [
+                        <button key={"btn-ctrl-1"} className={"button-control"} onClick={clickRandom}>Random</button>,
+                        <button key={"btn-ctrl-2"} className={"button-control"}>Advanced Search</button>
+                    ]
+                })
             })
     }, [])
 
     useEffect(() => {
         setPosterIsVisible(Array(list.length).fill(true))
-        // setInputOptions(list.map(
-        //     movie => {
-        //         return <option>{movie}</option>
-        //     }
-        // ))
     }, [list])
 
     useEffect(() => {
@@ -37,26 +38,32 @@ function MovieApp() {
             }))
     }, [posterIsVisible])
 
-    const onSearchInputChange = function (event) {
-        if (event.target.value === "") {
+    useEffect(() => {
+        if (searchBarValue === "") {
             setPosterIsVisible(Array(list.length).fill(true))
             return
         }
-        const regexString = RegExp(`.{0,}(${event.target.value}).{0,}`, "i")
+        // `.replace` on searchBarValue encodes regex special characters with backslashes
+        // https://stackoverflow.com/questions/874709/converting-user-input-string-to-regular-expression
+        const regexString = RegExp(`.*(${searchBarValue.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')}).*`, "i")
         setPosterIsVisible(list.map(item => regexString.test(item)))
+    }, [searchBarValue])
+
+    const clickRandom = function (event) {
+        setSearchBarValue(list[Math.floor(Math.random() * list.length)])
     }
 
     return (
         <div className="app">
-            <div className="search-bar">
+            <div className="controls-bar">
                 <h2>Hales Movie Archive</h2>
 
-                {/*<datalist id="search-options">*/}
-                {/*    {inputOptions}*/}
-                {/*</datalist>*/}
-                <input id="search-box" type="text" list="search-options" placeholder={`Search ${list.length} Movies`} onChange={onSearchInputChange}/>
+                <input id="search-box" type="text" placeholder={`Search ${list.length} Movies`}
+                       onChange={event => {
+                           setSearchBarValue(event.target.value)
+                       }} value={searchBarValue}/>
 
-                <div className="buttons-container">
+                <div className="button-controls">
                     {buttons}
                 </div>
             </div>
