6f33254449519d1c6fd8f91db5eccc2136281629
Riley Hales <rileyhales1@gmail.com>
2021-06-17 11:17:23 -0600

    fix posters not filtering

--- src/Gallery.js
+++ src/Gallery.js
@@ -5,19 +5,15 @@ import './Gallery.css';
 import MoviePoster from "./MoviePoster";
 
 const Gallery = function (props) {
-    const [posters, setPosters] = React.useState(null)
-    React.useEffect(() => {
-        const posterBaseUrl = `https://image.tmdb.org/t/p/w${props.imgHighRes ? "5" : "2"}00/`
-        setPosters(props.titlesList.map(
-            (movie, index) => {
-                return <MoviePoster key={index}
-                                    src={`${posterBaseUrl}${props.db[movie].poster}.jpg`}
-                                    visible={props.postersVisible[index]}
-                                    title={movie}
-                                    click={props.setSearchTitle}/>
-            })
-        )
-    }, [props.imgHighRes])
+    const posterBaseUrl = `https://image.tmdb.org/t/p/w${props.imgHighRes ? "5" : "2"}00/`
+    const posters = props.titlesList.map(
+        (movie, index) => {
+            return <MoviePoster key={index}
+                                src={`${posterBaseUrl}${props.db[movie].poster}.jpg`}
+                                visible={props.postersVisible[index]}
+                                title={movie}
+                                click={props.setSearchTitle}/>
+        })
 
     return (
         <div className="gallery">
